f2a2b51345fd18e7b8ecb5420277f95b0cf57d2f
Akshay Sangani <akshay@thetatechnolabs.com>
2021-03-03 15:49:07 +0530

    - ENV updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,4 +3,7 @@ export const environment = {
   cookieDomain: 'atunwapodcasts.com',
   base_url: 'https://atunwapodcasts.com:7000',
   IMG_URI : 'https://atunwapodcasts.com/api/'
+  // cookieDomain: 'devpub.atunwapodcasts.com',
+  // base_url: 'https://atunwapodcasts.com:7001',
+  // IMG_URI : 'https://devpub.atunwapodcasts.com/api/'
 };

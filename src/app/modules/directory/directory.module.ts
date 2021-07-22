c24e61c53c182993d020ef846503ce1829bce8be
leomaxi <leomaxi@outlook.com>
2021-07-22 11:39:56 +0000

    Using facebook share

--- src/app/modules/directory/directory.module.ts
+++ src/app/modules/directory/directory.module.ts
@@ -5,13 +5,17 @@ import { DirectoryComponent } from './directory.component';
 import { NgxSpinnerModule } from 'ngx-spinner';
 import { NgxUiLoaderModule } from 'ngx-ui-loader';
 import { NgxSocialShareModule } from 'ngx-social-share';
+import { ShareButtonsModule } from 'ngx-sharebuttons/buttons';
+import {ShareIconsModule} from 'ngx-sharebuttons/icons';
 @NgModule({
   imports: [
     CommonModule,
     DirectoryRoutingModule,
     NgxUiLoaderModule,
     NgxSpinnerModule,
-    NgxSocialShareModule
+    NgxSocialShareModule,
+    ShareButtonsModule,
+    ShareIconsModule
   ],
   declarations: [DirectoryComponent]
 })

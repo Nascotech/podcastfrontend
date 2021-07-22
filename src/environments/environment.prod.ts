8a40677444a114ffba7fe7714cd644929e0e1a7a
leomaxi <leomaxi@outlook.com>
2021-07-22 10:13:23 +0000

    change environment variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
-  cookieDomain: 'atunwapodcasts.com',
-  base_url: 'https://atunwapodcasts.com:7000',
-  IMG_URI : 'https://atunwapodcasts.com/api/'
+  cookieDomain: 'backup.atunwapodcasts.com',
+  base_url: 'https://backup.atunwapodcasts.com:7000',
+  IMG_URI : 'https://backup.atunwapodcasts.com/api/'
   // cookieDomain: 'devpub.atunwapodcasts.com',
   // base_url: 'https://atunwapodcasts.com:7001',
   // IMG_URI : 'https://devpub.atunwapodcasts.com/api/'
